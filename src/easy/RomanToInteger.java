8609e9ad9d29710c5a49a7f17d91a7b7f99a5177
pavlobatko <pavlobatko0@gmail.com>
2025-02-28 17:46:26 +0200

    reworked RomanToInteger to work with char

--- src/easy/RomanToInteger.java
+++ src/easy/RomanToInteger.java
@@ -4,24 +4,24 @@ import java.util.HashMap;
 
 public class RomanToInteger {
     public int romanToInt(String s) {
-        HashMap<String, Integer> romMap = new HashMap<>();
-        romMap.put("I", 1);
-        romMap.put("V", 5);
-        romMap.put("X", 10);
-        romMap.put("L", 50);
-        romMap.put("C", 100);
-        romMap.put("D", 500);
-        romMap.put("M", 1000);
+        HashMap<Character, Integer> romMap = new HashMap<>();
+        romMap.put('I', 1);
+        romMap.put('V', 5);
+        romMap.put('X', 10);
+        romMap.put('L', 50);
+        romMap.put('C', 100);
+        romMap.put('D', 500);
+        romMap.put('M', 1000);
 
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
             if (i < s.length() - 1
-                    && romMap.get(String.valueOf(s.charAt(i))) < romMap.get(String.valueOf(s.charAt(i + 1)))) {
-                result -= romMap.get(String.valueOf(s.charAt(i)));
+                    && romMap.get(s.charAt(i)) < romMap.get(s.charAt(i + 1))) {
+                result -= romMap.get(s.charAt(i));
             } else {
-                result += romMap.get(String.valueOf(s.charAt(i)));
+                result += romMap.get(s.charAt(i));
             }
         }
-        return result;
+        return result; // MCMXCIV = 1994 +1000 -100 +1000 -10 +100 -1 +5
     }
 }
