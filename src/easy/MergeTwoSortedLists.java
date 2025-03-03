55ead3aab703e5be55ba678aee8896f368da867d
pavlobatko <limyr15@gmail.com>
2025-03-03 14:40:15 +0200

    wqe

--- src/easy/MergeTwoSortedLists.java
+++ src/easy/MergeTwoSortedLists.java
@@ -1,7 +1,40 @@
 package easy;
 
-public class MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+import java.util.LinkedList;
 
+public class MergeTwoSortedLists {
+    public LinkedList mergeTwoLists(LinkedList list1, LinkedList list2) {
+//        if (list1 == null) {
+//            return list2;
+//        }
+//        if (list2 == null) {
+//            return list1;
+//        }
+//        if (list1.val <= list2.val) {
+//            list1.next = mergeTwoLists(list1.next, list2);
+//            return list1;
+//        } else {
+//            list2.next = mergeTwoLists(list1, list2.next);
+//            return list2;
+//        }
+//    }
+        return list1;
     }
 }
+
+//    ListNode res = new ListNode();
+//    ListNode curr = res;
+//        while( list1 != null && list2 != null) {
+//                if( list1.val <= list2.val) {
+//                curr.next = list1;
+//                list1 = list1.next;
+//                }
+//                else {
+//                curr.next = list2;
+//                list2 = list2.next;
+//                }
+//                curr = curr.next;
+//                }
+//                curr.next = list1 == null ? list2: list1;
+//                return res.next;
+
