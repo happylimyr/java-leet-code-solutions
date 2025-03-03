8483b9297ba8f44c971b4cbb5d02d570620499a6
pavlobatko <limyr15@gmail.com>
2025-03-03 15:01:24 +0200

    Revert "Reapply "Reapply "wqe"""
    This reverts commit 89117bf3415377e413cf7879254f52aca24e6477.

--- src/App.java
+++ src/App.java
@@ -1,19 +1,45 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-import easy.LongestCommonPrefix;
 import easy.RomanToInteger;
-import easy.ValidParentheses;
 
 public class App {
 
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        // char[] charA = a.toLowerCase().toCharArray();
+        // char[] charB = b.toLowerCase().toCharArray();
+
+        // Arrays.sort(charA);
+        // Arrays.sort(charB);
+
+        // return Arrays.equals(charA, charB);
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        int[] charCounts = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            charCounts[a.charAt(i) - 'a']++;
+            charCounts[b.charAt(i) - 'a']--;
+        }
+
+        for (int count : charCounts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        // RomanToInteger romanToInteger = new RomanToInteger();
-        // System.out.println(romanToInteger.romanToInt("MCMXCIV"));
-//LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
-//String[] input = { "dog", "racecar", "car", "race" };
-//System.out.println(longestCommonPrefix.longestCommonPrefix(input));
-        ValidParentheses vp = new ValidParentheses();
-        System.out.println(vp.isValid("({}[])"));
+        RomanToInteger romanToInteger = new RomanToInteger();
+        System.out.println(romanToInteger.romanToInt("MCMXCIV"));
     }
 }
