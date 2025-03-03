4295e45a520584d1d4d3d81296a8e31398fff72f
pavlobatko <limyr15@gmail.com>
2025-03-03 14:59:51 +0200

    Revert "Reapply "wqe""
    This reverts commit a99a163fa1b3adf6b38d5e8dedb95ea6e4918c29.

--- src/easy/MergeTwoSortedLists.java
+++ src/easy/MergeTwoSortedLists.java
@@ -1,40 +1,7 @@
 package easy;
 
-import java.util.LinkedList;
-
 public class MergeTwoSortedLists {
-    public LinkedList mergeTwoLists(LinkedList list1, LinkedList list2) {
-//        if (list1 == null) {
-//            return list2;
-//        }
-//        if (list2 == null) {
-//            return list1;
-//        }
-//        if (list1.val <= list2.val) {
-//            list1.next = mergeTwoLists(list1.next, list2);
-//            return list1;
-//        } else {
-//            list2.next = mergeTwoLists(list1, list2.next);
-//            return list2;
-//        }
-//    }
-        return list1;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
     }
 }
-
-//    ListNode res = new ListNode();
-//    ListNode curr = res;
-//        while( list1 != null && list2 != null) {
-//                if( list1.val <= list2.val) {
-//                curr.next = list1;
-//                list1 = list1.next;
-//                }
-//                else {
-//                curr.next = list2;
-//                list2 = list2.next;
-//                }
-//                curr = curr.next;
-//                }
-//                curr.next = list1 == null ? list2: list1;
-//                return res.next;
-
