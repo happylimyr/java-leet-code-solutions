b8f5018f1578a1ddd14d586b485fdde724615cda
pavlobatko <limyr15@gmail.com>
2025-03-03 21:38:00 +0200

    solution for FindIndexFirstOccurrence

--- src/easy/FindIndexFirstOccurrence.java
+++ src/easy/FindIndexFirstOccurrence.java
@@ -2,6 +2,6 @@ package easy;
 
 public class FindIndexFirstOccurrence {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+         return haystack.indexOf(needle);
     }
 }
